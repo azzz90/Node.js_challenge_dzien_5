942352a6ac80ba66f8ccb6ed6108752c9b97db81
azzz90 <aleksander.ziolkowski90@gmail.com>
2018-02-04 17:27:15 +0100

    zad 2

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,29 @@
 //Twój kod
+const express = require('express');
+const app = express();
+
+let name = ''
+
+app.get('/name/set/:imie', (req, res)=> {
+    name = req.params.imie;
+    res.send(`Twoje imię to: ${name}` );
+
+});
+
+app.get('/name/show', (req, res)=> {
+    res.send(`Sprawdzam czy Twoje imię to: ${name}` );
+
+});
+
+app.get('/name/check', (req, res)=> {
+    if (name.lenght !== 0) {
+    res.send(`Jakieś imię zostało wcześniej zapisane w programie` );
+  } else {
+    res.send('Nic nie zostało zapisane');
+  }
+
+});
+
+app.listen(3000, ()=> {
+  console.log('Serwer działa na porcie 3000');
+})
