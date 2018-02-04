6c81a3fca2b8ee47ba6f23e0f0b8d2b2ad787080
azzz90 <aleksander.ziolkowski90@gmail.com>
2018-02-04 16:58:14 +0100

    zad 1

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,14 @@
 //Twój kod
+const express = require('express');
+const app = express();
+
+app.get('/:numOne/:numTwo', (req,res)=>{
+  const numOne = req.params.numOne;
+  const numTwo = req.params.numTwo;
+  const Result = parseInt(numOne)+parseInt(numTwo);
+  res.send(`${numOne} + ${numTwo} = ${Result}`);
+});
+
+app.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
